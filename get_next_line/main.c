8e339b65e405524bec955326150a8ffe8cc9056c
LucasMarMed <lucas2marques@hotmail.com>
2023-07-15 00:36:12 +0100

    send files

---
+++ get_next_line/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumarque <lumarque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:18:08 by lumarque          #+#    #+#             */
+/*   Updated: 2023/07/08 15:00:27 by lumarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("text.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
